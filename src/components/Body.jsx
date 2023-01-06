1331a2ecd76d0c9077518f2d6b86b6b6bac0e020
mvn-anle-dn <an.le@monstar-lab.com>
2023-01-06 17:29:25 +0700

    Fix

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -9,34 +9,29 @@ import ShowCat from './ShowCat';
 export default function Body() {
   const url = 'https://api.thecatapi.com/v1/images/search?limit=20'
   const [list , setList] = useState([]) 
-  const [list2 , setList2] = useState([]) 
-  const [list3 , setList3] = useState([]) 
-  const [list4 , setList4] = useState([]) 
-  const [list5 , setList5] = useState([]) 
-  const [list6 , setList6] = useState([]) 
 
   const [image , setImage] = useState();
   const [slide , setSlide] = useState(false)
 
   useEffect(()=> {
-  
-    async function listCat(set) {
-      let getAPIlist = await axios.get(url)
-      let getlist = getAPIlist.data
-      set(getlist);
+    
+    async function listCat() {
+      let full = [] ;
+      for(let i = 0 ; i < 6 ; i++ ) {
+        let getAPIlist = await axios.get(url)
+        let getlist = getAPIlist.data
+        full = full.concat(getlist)
+        console.log(full);
+      }
+      setList(full);
     }
-    listCat(setList);
-    listCat(setList2);
-    listCat(setList3);
-    listCat(setList4);
-    listCat(setList5);
-    listCat(setList6);
+    listCat()
     
   },[])
 
-  const listAll = list.concat(list2,list3,list4,list5,list6);
+  const listAll = list
 
-  console.log(listAll);
+  
 
   const photos = listAll.map((photo) => ({
     src: photo.url,
